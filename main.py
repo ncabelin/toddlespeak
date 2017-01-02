de39967df683e4b149ea3c804a9b091f9f8fefea
Michael Cabelin <ncabelin@gmail.com>
2017-01-02 00:09:23 -0800

    add routes

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+# TO DO
+# 1. Create User Login
+# 2. Create User Signup
+# 3. 
+
 import random, string, datetime, os
 
 # 3rd party dependencies
@@ -15,6 +20,7 @@ from flask import make_response
 from itsdangerous import URLSafeSerializer
 import bleach
 from bcrypt import hashpw, checkpw, gensalt
+from functools import wraps
 
 import psycopg2
 
@@ -25,61 +31,90 @@ app = Flask(__name__)
 
 from sqlalchemy import create_engine, desc, or_
 from sqlalchemy.orm import sessionmaker
-from database import Base, User, Word, Goals
+from database import Base, User, Word, Goal
 
 engine = create_engine('postgresql://marco:marcopupu2014@localhost:5432/toddledatabase')
 DBSession = sessionmaker(bind = engine)
 session = DBSession()
 
-def find_word():
-	# find word
-	return
+def login_required(f):
+	@wraps(f)
+	def decorated_function(*args, **kwargs):
+		if 'username' in login_session:
+			return f(*args, **kwargs)
+		else:
+			return redirect(url_for('login'))
+	return decorated_function
 
 @app.route('/', methods=['GET'])
 def showHome():
 	# homepage
-	return
+	return render_template('home.html')
 
-@app.route('/signup', methods=['GET', 'POST'])
+@app.route('/signup', methods=['POST'])
+def signup():
 	# signup
-	return
+	return render_template('signup.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-	# login page
-	return
+	# login  and signup page 
+	return render_template('login.html')
 
+@app.route('/browse', methods=['GET'])
+def browse():
+	# main browsing page
+	return render_template('browse.html')
+
+@app.route('/summary', methods=['GET'])
+def summary():
+	return render_template('summary.html')
+
+@app.route('/help', methods=['GET'])
+def help():
+	return render_template('help.html')
+
+
+# Restful API Endpoints
+# --------------------------
 @app.route('/addword', methods=['POST'])
+@login_required
 def addWord():
 	# add word
 	return
 
 @app.route('/getwords', methods=['GET'])
+@login_required
 def getWords():
 	# get words in JSON format
 	return
 
-@app.route('/editword', methods=['GET'])
+@app.route('/editword', methods=['POST'])
+@login_required
 def editWord():
 	# edit word
 	return
 
 @app.route('/deleteword', methods=['DELETE'])
+@login_required
 def deleteWord():
 	# delete word
 	return
 
 @app.route('/addgoal', methods=['GET','POST'])
+@login_required
 def addGoal():
 	# add goal
 	return
 
 @app.route('/getgoals', methods=['GET','POST'])
+@login_required
 def getGoals():
 	# get goals
 	return
 
 @app.route('/editgoal', methods=['GET','POST'])
+@login_required
 def editGoal():
 	# edit goal
 	return
